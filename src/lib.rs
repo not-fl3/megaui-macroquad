5329db5bc62f03614a7b0fa351c5edfee8d3b5d6
ilya sheprut <optozorax@gmail.com>
2021-03-11 00:17:08 +0700

    input: fix bugs * fix double Enter * fix bug when Backspace or Enter not typed, because of upped immediately (e.g. programming keyboard can do that) by using `is_key_pressed` * use macro_rules to make code look prettier

--- src/lib.rs
+++ src/lib.rs
@@ -171,61 +171,37 @@ fn process_input() {
         }
     }
 
-    if is_key_down(KeyCode::Up) {
-        ctx.ui.key_down(megaui::KeyCode::Up, shift, ctrl);
+    macro_rules! process {
+        ($code:tt) => {
+            if is_key_pressed(KeyCode::$code) || is_key_down(KeyCode::$code) {
+                ctx.ui.key_down(megaui::KeyCode::$code, shift, ctrl);
+            }
+        };
     }
-    if is_key_down(KeyCode::Down) {
-        ctx.ui.key_down(megaui::KeyCode::Down, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Right) {
-        ctx.ui.key_down(megaui::KeyCode::Right, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Left) {
-        ctx.ui.key_down(megaui::KeyCode::Left, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Home) {
-        ctx.ui.key_down(megaui::KeyCode::Home, shift, ctrl);
-    }
-    if is_key_down(KeyCode::End) {
-        ctx.ui.key_down(megaui::KeyCode::End, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Delete) {
-        ctx.ui.key_down(megaui::KeyCode::Delete, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Backspace) {
-        ctx.ui.key_down(megaui::KeyCode::Backspace, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Enter) {
-        ctx.ui.key_down(megaui::KeyCode::Enter, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Tab) {
-        ctx.ui.key_down(megaui::KeyCode::Tab, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Z) {
-        ctx.ui.key_down(megaui::KeyCode::Z, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Y) {
-        ctx.ui.key_down(megaui::KeyCode::Y, shift, ctrl);
-    }
-    if is_key_down(KeyCode::C) {
-        ctx.ui.key_down(megaui::KeyCode::C, shift, ctrl);
-    }
-    if is_key_down(KeyCode::X) {
-        ctx.ui.key_down(megaui::KeyCode::X, shift, ctrl);
-    }
-    if is_key_down(KeyCode::V) {
-        ctx.ui.key_down(megaui::KeyCode::V, shift, ctrl);
-    }
-    if is_key_down(KeyCode::A) {
-        ctx.ui.key_down(megaui::KeyCode::A, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Escape) {
-        ctx.ui.key_down(megaui::KeyCode::Escape, shift, ctrl);
-    }
-    if is_key_down(KeyCode::Enter) {
-        ctx.ui.key_down(megaui::KeyCode::Enter, shift, ctrl);
-    }
-    if is_key_down(KeyCode::LeftControl) || is_key_down(KeyCode::RightControl) {
+
+    process!(Up);
+    process!(Down);
+    process!(Right);
+    process!(Left);
+    process!(Home);
+    process!(End);
+    process!(Delete);
+    process!(Backspace);
+    process!(Tab);
+    process!(Z);
+    process!(Y);
+    process!(C);
+    process!(X);
+    process!(V);
+    process!(A);
+    process!(Escape);
+    process!(Enter);
+
+    if is_key_down(KeyCode::LeftControl)
+        || is_key_down(KeyCode::RightControl)
+        || is_key_pressed(KeyCode::LeftControl)
+        || is_key_pressed(KeyCode::RightControl)
+    {
         ctx.ui.key_down(megaui::KeyCode::Control, shift, ctrl);
     }
     let (wheel_x, wheel_y) = mouse_wheel();
